aab07ce80a354908829a1d8ffa459ce74a4cecde
Nicholas Simpson <114123854+Nicholas-Simpson@users.noreply.github.com>
2023-07-13 18:26:41 -0700

    updating coding files and csv

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,47 +1,42 @@
-function createMap(){
+function createMap(HUDHousing){
     // Build tile layer for background
     let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
-
     let baseMaps = {
-        'Stree Map':streetmap
+        'Street Map': streetmap
     }
-
     let overlayMaps = {
-        "HUD Housing": ???
+        "HUD Housing": HUDHousing
     };
-
     let myMap = L.map("map", {
         center: [45.516682, -122.538490],
-        zoom: 12
+        zoom: 12,
+        layers: [streetmap, HUDHousing]
       });
     
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap)
-
-    function createMarkers() {
-        // pull hud locations from excel file 
-        let hudLocations = ;
-
-        // Initialize an array to hold hud locations
-        let hudMarkers = [];
-
-        // Loop through the hud locations 
-        for () {
-
-            // for each location create marker
-            let hudMarker = L.marker()
-                .bindPopup();
-
-            // add the marker to the hud array
-            hudLocations.push(hudMarker);
-            
-        }
-
-        // create a layer group made from hud locations and pass to createMap function
-        createMap(L.layerGroup())
-    }
-
 }
+function createMarkers(data) {
+    // Initialize an array to hold hud locations
+    let hudMarkers = [];
+    // Loop through the hud locations 
+    for (let i = 0; i < data.length; i++) {
+        let hud = data[i]
+        // for each location create marker
+        let hudMarker = L.marker([hud["Project's Latitude:"], hud["Project's Longitude:"]])
+            .bindPopup("<h3>Project Name:</h3><h5>" + hud["Project Name:"] + "</h5>");
+        // add the marker to the hud array
+        hudMarkers.push(hudMarker);
+        
+    }
+    // create a layer group made from hud locations and pass to createMap function
+    createMap(L.layerGroup(hudMarkers))
+}
+d3.csv("../../Resources/LIHTCdatasetlatlong.csv").then((data) => {
+    console.log(data)
+    createMarkers(data)
+}
+)
